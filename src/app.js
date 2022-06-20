906ff783f9a7fc33b6cd4c33b74d72e36b9e3836
Godwin254 <godwinbee15@gmail.com>
2022-06-21 01:04:15 +0300

    add fetch data from api and render to DOM

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@
 
     //events
     window.addEventListener('DOMContentLoaded', () => {
+        //document.querySelector("#demo")
+        console.log("connected");
+        fetchData(); //start fetch data
 
     })
 
@@ -13,22 +16,32 @@
         fetch (`https://api.json-generator.com/templates/dkGUFM9yecgO/data?access_token=8ziyqp1pbfk3vid625oroxgvsh5yzzb7m9lqcadk`)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            //console.log(data);
+            data.forEach( elem => {
+                elem.recipes.forEach(recipe => {
+                    console.log(recipe);
+                    recipes(recipe);
+                });
+                
+
+            })
+            
         })
         .catch(err => console.error(err));
     }
 
     //create recipe post
-    const recipes = (dt) => {
-
+    const recipes = (rc) => {
+        let demo = document.getElementById("demo");
         let output = '';
+        //console.log(demo)
 
         output += `
                     <div class ="recipes">
                         <div class  = "recipe">
-                            <img src = ${dt} alt="recipe" width="300px">
-                            <h5> ${dt} </h5>
-                            <p> ${dt} </p>
+                            <img src = ${rc.desc} alt="recipe" width="300px">
+                            <h5> ${rc.title} </h5>
+                            <p> ${rc.desc} </p>
                             <div class =ctrls>
                                 <span class = "like"> Like </span>
                                 <input type="button" value ="read more" />
@@ -39,6 +52,7 @@
                 `
         
         //update dom output
+        demo.innerHTML = output;
     }
     
 })();
