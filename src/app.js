7baf4b0778280564441a8f4ec2e4d86df35617fd
Godwin254 <godwinbee15@gmail.com>
2022-06-20 23:57:37 +0300

    add fetch api implementation

--- src/app.js
+++ src/app.js
@@ -4,4 +4,12 @@
     //events
 
     //method
+    
+    //fetch data --api
+    fetch (`https://api.json-generator.com/templates/dkGUFM9yecgO/data?access_token=8ziyqp1pbfk3vid625oroxgvsh5yzzb7m9lqcadk`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(err => console.error(err));
 })();
